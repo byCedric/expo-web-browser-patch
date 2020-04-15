0a46eda284a34d9038ab6ab6880faea00f2759ff
Cedric van Putten <me@bycedric.com>
2020-04-15 19:05:03 +0200

    feat: setup example that fails without patch in production mode

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
+import { getAuthorizeUrl } from './helpers';
 
 export default function App() {
+  const startAuth = useCallback(async () => {
+    const returnUrl = AuthSession.getDefaultReturnUrl();
+    const authUrl = getAuthorizeUrl(
+      {
+        client_id: 'INSERT_YOUR_CLIENT_ID', // https://www.reddit.com/prefs/apps/ + match redirect Url with aformentioned returnUrl
+        duration: 'temporary',
+        redirect_uri: returnUrl,
+        response_type: 'code',
+        scope: ['identity'],
+      },
+      '.compact'
+    );
+
+    console.log('starting auth session');
+    try {
+      const result = await AuthSession.startAsync({ authUrl, returnUrl });
+      // Without the patch, this is called too early in production mode
+      // see: https://github.com/expo/expo/pull/6743
+      console.log('result received', result);
+    } catch (error) {
+      console.log('error received', error);
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title='Press me' onPress={startAuth} />
     </View>
   );
 }
